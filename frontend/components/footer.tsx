f3c9a56c8f00117a86d901a2f9807278d71e9d86
Wardlexy <lexdwardo@gmail.com>
2026-05-25 11:22:57 +0700

    chore: simplify footer copy

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.copy}>Copyright &copy; 2026. All rights reserved.</p>
+      <p className={styles.copy}>&copy; 2026. All rights reserved.</p>
       <p className={styles.brand}>NutriCheck</p>
     </footer>
   );
